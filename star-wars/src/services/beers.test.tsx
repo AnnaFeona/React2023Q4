068cbc237b1517092afe1c833e16f870402bf4d3
AnnaFeona <glavatskikh.anna@gmail.com>
2023-11-19 23:27:18 +0300

    test: fix beersApi test

--- star-wars/src/services/beers.test.tsx
+++ star-wars/src/services/beers.test.tsx
@@ -4,18 +4,22 @@ import { store } from '../store/store';
 import { Provider } from 'react-redux';
 import App from '../App/App';
 import { server } from '../mocks/server';
+import * as useGetBeerByNameQuery from './beers';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('beerApi queries', () => {
-  it('should fetch beer by name and per_page', async () => {
+  it('should fetch beer by name', async () => {
+    const spyAPIcall = jest.spyOn(useGetBeerByNameQuery, 'useGetBeerByNameQuery');
     render(
       <Provider store={store}>
         <App />
       </Provider>,
     );
+
+    expect(spyAPIcall).toHaveBeenCalled();
 
     await waitFor(() => {
       expect(screen.getByText(/coldbeer/i)).toBeInTheDocument();
