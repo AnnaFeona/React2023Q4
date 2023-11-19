42a9879c4e1f21ec6c72923289e026a54f67bd98
AnnaFeona <glavatskikh.anna@gmail.com>
2023-11-19 14:04:31 +0300

    test: add mock server

---
+++ star-wars/src/mocks/handlers.ts
@@ -0,0 +1,21 @@
+import { HttpResponse, http } from 'msw';
+import { API_BASE_URL } from '../model/constants';
+import { allBeers, mockBeerList } from './cardListMock';
+
+export const handlers = [
+  http.get(`${API_BASE_URL}`, ({ request }) => {
+    const url = new URL(request.url);
+
+    const perPage = url.searchParams.get('per_page');
+
+    if (perPage === '80') return HttpResponse.json(allBeers);
+
+    return HttpResponse.json(mockBeerList);
+  }),
+
+  http.get(`${API_BASE_URL}/:id`, ({ params }) => {
+    const { id } = params;
+    const result = mockBeerList.filter((item) => item.id.toString() === id);
+    return HttpResponse.json(result);
+  }),
+];
--- star-wars/src/services/beers.test.tsx
+++ star-wars/src/services/beers.test.tsx
@@ -1,24 +1,9 @@
 import '@testing-library/jest-dom/extend-expect';
-import { http, HttpResponse } from 'msw';
-import { setupServer } from 'msw/node';
 import { render, screen, waitFor } from '@testing-library/react';
-import { API_BASE_URL } from '../model/constants';
-import { mockBeerList } from '../mocks/cardListMock';
 import { store } from '../store/store';
 import { Provider } from 'react-redux';
 import App from '../App/App';
-
-const server = setupServer(
-  http.get(`${API_BASE_URL}`, () => {
-    return HttpResponse.json(mockBeerList);
-  }),
-
-  http.get(`${API_BASE_URL}/:id`, ({ params }) => {
-    const { id } = params;
-    const result = mockBeerList.filter((item) => item.id.toString() === id);
-    return HttpResponse.json(result);
-  }),
-);
+import { server } from '../mocks/server';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
