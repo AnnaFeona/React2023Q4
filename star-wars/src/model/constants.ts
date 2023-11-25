dbce981c3f8489f029a2e93936cef571e738a379
AnnaFeona <glavatskikh.anna@gmail.com>
2023-11-25 13:15:26 +0300

    refactor: remove star-wars folder

--- star-wars/src/model/constants.ts
+++
@@ -1,5 +0,0 @@
-export const API_BASE_URL = `https://api.punkapi.com/v2/beers`;
-export const STORAGE_KEY_PREFFIX = `rs-react-star-wars`;
-
-export const INITIAL_PAGE = 1;
-export const INITIAL_LIMIT = 10;
--- star-wars/src/pages/notFound/notFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-export const NotFoundPage: FC = () => {
-  return (
-    <>
-      <div className="not-found">404. Page not found</div>
-    </>
-  );
-};
