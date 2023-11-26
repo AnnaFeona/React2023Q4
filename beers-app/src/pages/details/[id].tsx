d94d29048b38a979c1c2957eb3f243878a995357
AnnaFeona <glavatskikh.anna@gmail.com>
2023-11-26 03:04:05 +0300

    feat: add gSSR to details page

--- beers-app/src/pages/details/[id].tsx
+++ beers-app/src/pages/details/[id].tsx
@@ -1,5 +1,17 @@
 import { useRouter } from 'next/router';
 import { Details } from '../../components/screens/details/details';
+import { beerApi } from '../../services/beers';
+import { wrapper } from '../../store/store';
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ query }) => {
+  console.log(query);
+
+  store.dispatch(beerApi.endpoints.getBerrById.initiate('1'));
+
+  Promise.all(store.dispatch(beerApi.util.getRunningQueriesThunk()));
+
+  return { props: { id: 0 } };
+});
 
 const DetailsPage = () => {
   const { query } = useRouter();
